0020ef837a0c242a83a76ce658b49c1204889127
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-04 23:08:00 -0300

    Update

--- Js/Alerts.js
+++ Js/Alerts.js
@@ -1,11 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Esconde os alertas quando a página é carregada
+    document.querySelector('.alert-success').style.display = 'none';
+    document.querySelector('.alert-danger').style.display = 'none';
+});
+
 function showAlert(isValid) {
     // Seleciona os elementos de alerta
     var alertSuccess = document.querySelector('.alert-success');
-    console.log(alertSuccess)
     var alertDanger = document.querySelector('.alert-danger');
-
-    alertSuccess.classList.add('alert-fixed-bottom-right');
-    alertDanger.classList.add('alert-fixed-bottom-right');
 
     // Esconde ambos os alertas
     alertSuccess.style.display = 'none';
@@ -17,21 +19,26 @@ function showAlert(isValid) {
         alertSuccess.textContent = 'Ip informado está correto!';
         // Inicia a animação para desaparecer após 3 segundos
         setTimeout(() => {
-            alertSuccess.classList.add('alert-fade-out');
-        }, 3000);
+            alertSuccess.style.display = 'none';
+        }, 2000);
     } else {
         alertDanger.style.display = 'block';
         alertDanger.textContent = 'Ip informado está inválido!';
         // Inicia a animação para desaparecer após 3 segundos
         setTimeout(() => {
-            alertDanger.classList.add('alert-fade-out');
-        }, 3000);
+            alertDanger.style.display = 'none';
+        }, 2000);
     }
 }
 
 // Regex para validar o IP
 const regex = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$/;
 
+// Seleciona o campo de entrada
+const input_Search = document.querySelector(".input-field");
 
-// Chama a função showAlert com o resultado da validação
-showAlert(regex.test(input_Search));
+// Adiciona um ouvinte de evento para detectar cada entrada do usuário
+input_Search.addEventListener('input', function() {
+    // Chama a função showAlert com o resultado da validação
+    showAlert(regex.test(input_Search.value));
+});
