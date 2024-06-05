03c4aa93c1427ed5d363ac5c401924e3a7f56686
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-05 09:14:23 -0300

    Update Index.html, Index.js

--- Js/index.js
+++ Js/index.js
@@ -1,10 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const Button_search = document.querySelector(".submit-btn");
 
-const Button_search = document.querySelector(".submit-btn")
+    Button_search.addEventListener("click", function () {
+        const input_Search = document.querySelector(".input-field").value;
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_vpHUuVP400WqsLHAC0QpySyGsbm31&ipAddress=${input_Search}`)
+            .then((response) => response.json())
+            .then((data) => {
+             
+                const ipAddressField = document.querySelector(".ipAddressField");
+                const locationInput = document.querySelector(".locationInput");
+                const timezoneInput = document.querySelector(".timezoneInput");
+                const ispInput = document.querySelector(".ispInput");
 
-Button_search.addEventListener("click", function () {
-    const input_Search = document.querySelector(".input-field").value
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_vpHUuVP400WqsLHAC0QpySyGsbm31&ipAddress=${input_Search}`)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.log(error))
-})
+                ipAddressField.innerHTML = data.ip
+                locationInput.innerHTML = data.location.city
+                timezoneInput.innerHTML = data.location.timezone
+                ispInput.innerHTML = data.isp
+                
+            })
+    });
+});
