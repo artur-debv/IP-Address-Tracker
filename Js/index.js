ce4e862a5e887692dae2f1cf3af68e8a7fccdd9b
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-06-05 10:13:39 -0300

    update

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const Button_search = document.querySelector(".submit-btn");
 
@@ -6,17 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
         fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_vpHUuVP400WqsLHAC0QpySyGsbm31&ipAddress=${input_Search}`)
             .then((response) => response.json())
             .then((data) => {
-             
                 const ipAddressField = document.querySelector(".ipAddressField");
                 const locationInput = document.querySelector(".locationInput");
                 const timezoneInput = document.querySelector(".timezoneInput");
                 const ispInput = document.querySelector(".ispInput");
 
-                ipAddressField.innerHTML = data.ip
-                locationInput.innerHTML = data.location.city
-                timezoneInput.innerHTML = data.location.timezone
-                ispInput.innerHTML = data.isp
-                
-            })
+                ipAddressField.innerHTML = data.ip;
+                locationInput.innerHTML = data.location.city;
+                timezoneInput.innerHTML = data.location.timezone;
+                ispInput.innerHTML = data.isp;
+
+               
+            });
     });
 });
